0f5c203632540cd15f0c70a2624c25ebceec9478
wangbocong <y541322460@163.com>
2022-05-01 22:23:03 +0800

    1

--- src/main/java/com/labor/LaborApplication.java
+++ src/main/java/com/labor/LaborApplication.java
@@ -1,11 +1,9 @@
 package com.labor;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.labor.mapper.UserMapper")
 public class LaborApplication {
 
     public static void main(String[] args) {
