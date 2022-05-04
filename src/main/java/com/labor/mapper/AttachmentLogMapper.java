1e337c980664b5d31ad76c660d9d6ed9bd78273a
wangbocong <y541322460@163.com>
2022-05-04 13:42:32 +0800

    文件上传日志

---
+++ src/main/java/com/labor/mapper/AttachmentLogMapper.java
@@ -0,0 +1,13 @@
+package com.labor.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.labor.entity.AttachmentLog;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author BoCong
+ * @date 2022/5/4
+ */
+@Mapper
+public interface AttachmentLogMapper extends BaseMapper<AttachmentLog> {
+}
